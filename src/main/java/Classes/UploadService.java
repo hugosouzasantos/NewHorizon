c1f8dcb76ebe0907e421f2b28ad4f56eac565471
mthlssn <thalissonribeirodossantos05@hmail.com>
2021-09-09 19:58:10 -0300

    Primeiro Commit

---
+++ src/main/java/Classes/UploadService.java
@@ -0,0 +1,39 @@
+package Classes;
+
+import java.io.File;
+import java.util.List;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+public class UploadService {
+	
+	private String diretorio;
+	private String nomeArquivo;
+	
+	private String nomeOriginal;
+	private File arquivoTemp = null;
+	
+	public UploadService(String diretorio) {
+		this.diretorio = diretorio; 
+	}
+	
+	public boolean doUpload(HttpServletRequest request, ServletContext context){
+		if(ServletFileUpload.isMultipartContent(request)) {
+			DiskFileItemFactory factory = new DiskFileItemFactory();
+			ServletFileUpload upload = new ServletFileUpload(factory);
+			String caminhoDiretorio = context.getRealPath("") + File.separator + diretorio;
+			
+			File diretorioUpload = new File(caminhoDiretorio);
+			
+			if(!diretorioUpload.exists()) {
+				diretorioUpload.mkdir();
+			}
+		}
+		return false;
+		
+	}
+	
+}
